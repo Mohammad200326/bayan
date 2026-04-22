1d630bf7b1bfb8aab6bd6b85f95b3bc97703b8f6
Mohammad200326 <92891827+Mohammad200326@users.noreply.github.com>
2026-04-22 04:12:09 +0200

    feat: book page with vapi

---
+++ lib/actions/session.actions.ts
@@ -0,0 +1,66 @@
+"use server";
+
+import VoiceSession from "@/database/models/voiceSession.model";
+import { connectToDatabase } from "@/database/mongoose";
+import { EndSessionResult, StartSessionResult } from "@/types";
+import { getCurrentBillingPeriodStart } from "../subscription-constants";
+
+export const startVoiceSession = async (
+  clerkId: string,
+  bookId: string,
+): Promise<StartSessionResult> => {
+  try {
+    await connectToDatabase();
+
+    //
+
+    const session = await VoiceSession.create({
+      clerkId,
+      bookId,
+      startedAt: new Date(),
+      billingPeriodStart: getCurrentBillingPeriodStart(),
+      durationSeconds: 0,
+    });
+
+    return {
+      success: true,
+      sessionId: session._id.toString(),
+    };
+  } catch (error) {
+    console.error("Error starting voice session:", error);
+    return {
+      success: false,
+      error: "حدث خطأ أثناء بدء الجلسة الصوتية. حاول مرة أخرى.",
+    };
+  }
+};
+
+export const endVoiceSession = async (
+  sessionId: string,
+  durationSeconds: number,
+): Promise<EndSessionResult> => {
+  try {
+    await connectToDatabase();
+
+    const result = await VoiceSession.findByIdAndUpdate(sessionId, {
+      endedAt: new Date(),
+      durationSeconds,
+    });
+
+    if (!result)
+      return {
+        success: false,
+        error: "الجلسة غير موجودة.",
+      };
+
+    return {
+      success: true,
+    };
+  } catch (error) {
+    console.error("Error ending voice session:", error);
+    return {
+      success: false,
+      error: "حدث خطأ أثناء إنهاء الجلسة الصوتية. حاول مرة أخرى.",
+    };
+  }
+};
