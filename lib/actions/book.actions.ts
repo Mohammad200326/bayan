c4abc1ee635c9368b449207fc20d17aa988cc841
Mohammad200326 <92891827+Mohammad200326@users.noreply.github.com>
2026-04-22 17:20:16 +0200

    fix deployment issues

--- lib/actions/book.actions.ts
+++ lib/actions/book.actions.ts
@@ -6,6 +6,7 @@ import { escapeRegex, generateSlug, serializeData } from "../utils";
 import Book from "@/database/models/book.model";
 import BookSegment from "@/database/models/bookSegment.model";
 import mongoose from "mongoose";
+import { revalidatePath } from "next/cache";
 
 export const checkBookExists = async (title: string) => {
   try {
@@ -53,6 +54,9 @@ export const createBook = async (data: CreateBook) => {
     //
 
     const book = await Book.create({ ...data, slug, totalSegments: 0 });
+
+    revalidatePath("/");
+
     return {
       success: true,
       data: serializeData(book),
