fb01d8c0f35606472ba8ca9f0e3801316240700c
Mohammad200326 <92891827+Mohammad200326@users.noreply.github.com>
2026-04-23 04:52:21 +0200

    feat: search in books

--- lib/actions/book.actions.ts
+++ lib/actions/book.actions.ts
@@ -140,12 +140,21 @@ export const saveBookSegments = async (
   }
 };
 
-export const getAllBooks = async () => {
+export const getAllBooks = async (search?: string) => {
   try {
     await connectToDatabase();
 
-    const books = await Book.find().sort({ createdAt: -1 }).lean();
+    let query = {};
 
+    if (search) {
+      const escapedSearch = escapeRegex(search);
+      const regex = new RegExp(escapedSearch, "i");
+      query = {
+        $or: [{ title: { $regex: regex } }, { author: { $regex: regex } }],
+      };
+    }
+
+    const books = await Book.find(query).sort({ createdAt: -1 }).lean();
     return {
       success: true,
       data: serializeData(books),
